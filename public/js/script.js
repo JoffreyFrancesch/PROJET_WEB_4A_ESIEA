046107a4ecb61ac7aee556eddf76e8f144d3a5b7
Joffrey Franceschini <joffrey@MacBook-Pro-de-Joffrey.local>
2018-09-07 15:57:09 +0200

    nouveau repo mauvais uttilisateur qui push

---
+++ public/js/script.js
@@ -0,0 +1,154 @@
+Vue.component('modal', {
+    template: '#modal-template',
+    data : () => ({
+        newUser: {
+            firstName: '',
+            lastName: '',
+            date: '',
+            email: '',
+            password: ''
+        },
+        userTemp: {
+            email: '',
+            password: '',
+        }
+    }),
+    methods : {
+        addUser : function (){
+            console.log('addUser')
+            this.$emit('adduser', this.newUser);
+        },
+        logUser : function (){
+            console.log('LogUser')
+            this.$emit('loguser', this.userTemp);
+        }
+    }
+});
+
+Vue.prototype.$http = axios;
+
+const app = new Vue({
+    el: "#app",
+    data: {
+        currentPage: "menu",
+        showModal: false,
+        stockClient: [],
+        cartClient: [],
+        user : {
+            email : ''
+        }
+    },
+    methods: {
+        addToCart: function (index) {
+            this.$http.post('/myCart', {
+                    id: this.stockClient[index].id
+                })
+                .then((response) => {
+                    if (response.data === "OK") {
+                        this.cartClient[index].quantity += 1;
+                        this.stockClient[index].quantity -= 1;
+                    } else {
+                        alert(response.data);
+                    }
+                })
+                .catch((err) => {
+                    console.log(err);
+                })
+        },
+        //fonction de changement du text si le produit n'est plus en stock
+        textChange: function (index) {
+            if (this.stockClient[index].quantity === 0) {
+                this.stockClient[index].btn = "Victime de son succes";
+            }
+            if (this.stockClient[index].quantity >= 1) {
+                this.stockClient[index].btn = "Ajoutez au panier";
+            }
+        },
+        // calcul le total prix total de chaque article en fonction du nb demandé
+        semiTotal: function (index) {
+            return this.cartClient[index].quantity * this.cartClient[index].price;
+        },
+        // supprime l'article du panier si nous le souhaitons
+        deleteItem: function (index) {
+            this.$http.post('/myStock', {
+                    id: this.cartClient[index].id,
+                })
+                .then((response) => {
+                    if (response.data === "OK") {
+                        this.cartClient[index].quantity -= 1;
+                        this.stockClient[index].quantity += 1;
+                    } else {
+                        alert(response.data)
+                    }
+                })
+                .catch((err) => {
+                    console.log(err);
+                })
+        },
+        onAddUser: function (newUser) {
+            this.$http.post('/createUser', {
+                firstName: newUser.firstName,
+                lastName: newUser.lastName,
+                date: newUser.date,
+                email: newUser.email,
+                password: newUser.password
+            })
+            .then((response) => {
+                this.user.email = newUser.email
+                alert(response.data);
+            })
+        },
+        onLogUser: function (user) {
+            this.$http.post('/login', {
+                email : user.email,
+                password : user.password
+            })
+            .then((response) => {
+                if(response.data === "OK"){
+                    this.user.email = user.email
+                    console.log("Connecte")
+                } else {
+                    alert(response.data);
+                }
+            })
+        },
+        paiement: function (){
+            var total = 0;
+            this.cartClient.forEach(element => {
+              total += element.quantity * element.price;
+            });
+            if(this.user.email != ''){
+                if(total != 0){
+                    alert("Paiement réussi");
+                } else {
+                    alert("Panier vide");
+                }
+            
+            } else {
+                alert("Veuillez vous connectez");
+            }
+        },
+    },
+    computed: {
+        // simple fonction de calcul du montant total de tout les articles du panier
+        totalCart: function () {
+            var sum = 0;
+            this.cartClient.forEach(element => {
+                sum += element.quantity * element.price;
+            });
+            return sum;
+        }
+    },
+    created() {
+        this.$http.get('/myStock')
+            .then((response) => {
+                console.log('reponse du stock serveur', response.data)
+                this.stockClient = response.data;
+            });
+        this.$http.get('myCart')
+            .then((response) => {
+                console.log("reponse du panier serveur", response.data);
+                this.cartClient = response.data;
+            });
+    }
+});
---
+++ routes/index.js
@@ -0,0 +1,170 @@
+var express = require('express');
+var router = express.Router();
+
+var stockServer = [{
+        id: 0,
+        name: "Globemaster",
+        description: "Pour toute les occasions",
+        price: 17000,
+        src: "assets/omega_front.png",
+        btn: "Ajoutez au panier",
+        quantity: 5
+    },
+    {
+        id: 1,
+        name: "De Ville",
+        description: "Le style casual par excellence",
+        price: 11000,
+        src: "assets/omega_2_front.png",
+        btn: "Ajoutez au panier",
+        quantity: 5
+    },
+    {
+        id: 2,
+        name: "Seamaster",
+        description: "Le chic à l'état pur",
+        price: 36000,
+        src: "assets/omega_3_front.png",
+        btn: "Ajoutez au panier",
+        quantity: 5
+    },
+    {
+        id: 3,
+        name: "Speedmaster",
+        description: "La montre des aventurier",
+        price: 4000,
+        src: "assets/omega_4_front.png",
+        btn: "Ajoutez au panier",
+        quantity: 5
+    },
+    {
+        id: 4,
+        name: "Seamaster",
+        description: "La montre de 007",
+        price: 15000,
+        src: "assets/omega_5_front.png",
+        btn: "Ajoutez au panier",
+        quantity: 5
+    },
+    {
+        id: 5,
+        name: "MoonWatch",
+        description: "La montre des Astronautes",
+        price: 5000,
+        src: "assets/omega_6_front.png",
+        btn: "Ajoutez au panier",
+        quantity: 5
+    }
+]
+
+var cartServer = [{
+    id: 0,
+    quantity: 0,
+    name: "Globemaster",
+    price: 17000,
+    src: "assets/omega_front.png"
+}, {
+    id: 1,
+    price: 11000,
+    src: "assets/omega_2_front.png",
+    quantity: 0
+}, {
+    id: 2,
+    name: "Seamaster",
+    price: 36000,
+    src: "assets/omega_3_front.png",
+    quantity: 0
+}, {
+    id: 3,
+    name: "Speedmaster",
+    price: 4000,
+    src: "assets/omega_4_front.png",
+    quantity: 0
+}, {
+    id: 4,
+    name: "Seamaster",
+    price: 15000,
+    src: "assets/omega_5_front.png",
+    quantity: 0
+}, {
+    id: 5,
+    name: "MoonWatch",
+    price: 5000,
+    src: "assets/omega_6_front.png",
+    quantity: 0
+}]
+
+var users = [{
+    firstname : "joffrey",
+    lastname : "franceschini",
+    email : "franceschini@et.esiea.fr",
+    password : "pauline",
+    date : "02/03/1997"
+}]
+
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+    res.render('index', {
+        title: 'Express'
+    });
+});
+
+router.get('/myStock', (req, res) => {
+    res.json(stockServer)
+});
+
+router.get('/myCart', (req, res) => {
+    res.json(cartServer)
+});
+
+
+router.post('/myCart', (req, res) => {
+    var id = req.body.id;
+    if (stockServer[id].quantity >= 1) {
+        cartServer[id].quantity += 1;
+        stockServer[id].quantity -= 1;
+        res.status(200).send("OK");
+    } else {
+        res.send("L'article n'est pas en stock");
+    }
+})
+
+router.post('/myStock', (req,res) =>{
+    var id = req.body.id;
+    console.log('id', id)
+    if(cartServer[id].quantity >= 1){
+        cartServer[id].quantity -= 1;
+        stockServer[id].quantity += 1;
+        res.status(200).send("OK");
+    } else {
+        res.send("L'item n'était pas dans le panier");
+    }
+})
+
+
+router.post('/createUser', (req, res) => {
+    users.push({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        date: req.body.date,
+        email: req.body.email,
+        password: req.body.password
+    })
+    res.status(200).send('User enregistré');
+})
+
+router.post('/login', (req,res) =>{
+    var user = req.body.email;
+    var password = req.body.password;
+    users.forEach(item =>{
+        if(item.email === user && item.password === password){
+            res.status(200).send("OK");
+        } else {
+            res.send("Uttilisateur inconnu");
+        }
+    })
+
+})
+
+module.exports = router;

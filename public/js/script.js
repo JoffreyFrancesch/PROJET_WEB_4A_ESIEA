4a19101318d70e4047c0adb2ab5f36e4a8f28b35
JoffreyFrancesch <franceschini@et.esiea.fr>
2018-09-10 13:39:57 +0200

    add some comments

--- public/js/script.js
+++ public/js/script.js
@@ -1,28 +1,33 @@
-Vue.component('modal', {
-    template: '#modal-template',
-    data : () => ({
-        newUser: {
-            firstName: '',
-            lastName: '',
-            date: '',
-            email: '',
-            password: ''
-        },
-        userTemp: {
-            email: '',
-            password: '',
-        }
-    }),
-    methods : {
-        addUser : function (){
-            console.log('addUser')
-            this.$emit('adduser', this.newUser);
-        },
-        logUser : function (){
-            console.log('LogUser')
-            this.$emit('loguser', this.userTemp);
-        }
+// component pour le modal uttilisé pour le login
+Vue.component("modal", {
+  template: "#modal-template",
+  data: () => ({
+    // object contenant toute les infos d'un nouvel user
+    newUser: {
+      firstName: "",
+      lastName: "",
+      date: "",
+      email: "",
+      password: ""
+    },
+    // object contennt les infos pour un user connu
+    userTemp: {
+      email: "",
+      password: ""
     }
+  }),
+  methods: {
+    // fonction qui permet d'appeler la fonction pour ajouter un user
+    addUser: function() {
+      console.log("addUser");
+      this.$emit("adduser", this.newUser);
+    },
+    // fonction qui permet d'appeler la fonction pour connecter un user
+    logUser: function() {
+      console.log("LogUser");
+      this.$emit("loguser", this.userTemp);
+    }
+  }
 });
 
 Vue.prototype.$http = axios;
@@ -32,13 +37,14 @@ const app = new Vue({
     data: {
         currentPage: "menu",
         showModal: false,
-        stockClient: [],
-        cartClient: [],
-        user : {
+        stockClient: [], // liste stock cote client
+        cartClient: [], // liste panier cote client
+        user : { // retiens l'email du user pour l'afficher
             email : ''
         }
     },
     methods: {
+        // function pour ajouter au panier Local et informer le serveur
         addToCart: function (index) {
             this.$http.post('/myCart', {
                     id: this.stockClient[index].id
@@ -55,20 +61,11 @@ const app = new Vue({
                     console.log(err);
                 })
         },
-        //fonction de changement du text si le produit n'est plus en stock
-        textChange: function (index) {
-            if (this.stockClient[index].quantity === 0) {
-                this.stockClient[index].btn = "Victime de son succes";
-            }
-            if (this.stockClient[index].quantity >= 1) {
-                this.stockClient[index].btn = "Ajoutez au panier";
-            }
-        },
         // calcul le total prix total de chaque article en fonction du nb demandé
         semiTotal: function (index) {
             return this.cartClient[index].quantity * this.cartClient[index].price;
         },
-        // supprime l'article du panier si nous le souhaitons
+        // supprime l'article du panier si nous le souhaitons et informe le serveur
         deleteItem: function (index) {
             this.$http.post('/myStock', {
                     id: this.cartClient[index].id,
@@ -85,20 +82,25 @@ const app = new Vue({
                     console.log(err);
                 })
         },
+        // ajoute un compte uttilisateur et le transmet a l'uttilisateur
         onAddUser: function (newUser) {
-            
-            this.$http.post('/createUser', {
-                firstName: newUser.firstName,
-                lastName: newUser.lastName,
-                date: newUser.date,
-                email: newUser.email,
-                password: newUser.password
-            })
-            .then((response) => {
-                this.user.email = newUser.email
-                alert(response.data);
-            })
+            if(newUser.firstName != '' && newUser.lastName != '' && newUser.date != '' && newUser.date != '' && newUser.email != '' && newUser.password != ''){
+                this.$http.post('/createUser', {
+                    firstName: newUser.firstName,
+                    lastName: newUser.lastName,
+                    date: newUser.date,
+                    email: newUser.email,
+                    password: newUser.password
+                })
+                    .then((response) => {
+                        this.user.email = newUser.email
+                        alert(response.data);
+                    })
+            } else {
+                alert('Il manque des infos...');
+            }
         },
+        // connexion d'un uttilisateur connu
         onLogUser: function (user) {
             this.$http.post('/login', {
                 email : user.email,
@@ -113,6 +115,7 @@ const app = new Vue({
                 }
             })
         },
+        // réalise un paiement fictif pour montrer que cela fonctionne bien
         paiement: function (){
             var total = 0;
             this.cartClient.forEach(element => {
@@ -120,13 +123,12 @@ const app = new Vue({
             });
             if(this.user.email != ''){
                 if(total != 0){
-                    alert("Paiement réussi");
+                    alert("Paiement réussi : le compte FRXX XXXX XXXX XXXX XXX 87 sera bien débité de la somme de " + this.total + " € ");
                 } else {
-                    alert("Panier vide");
+                    alert("Le panier est vide, le compte FRXX XXXX XXXX XXXX XXX 87 ne sera donc pas débité");
                 }
-            
             } else {
-                alert("Veuillez vous connectez");
+                alert("Veuillez vous connectez pour procéder au paiement");
             }
         },
     },
@@ -141,11 +143,13 @@ const app = new Vue({
         }
     },
     created() {
+        // récupère la liste des stocks du serveur
         this.$http.get('/myStock')
             .then((response) => {
                 console.log('reponse du stock serveur', response.data)
                 this.stockClient = response.data;
             });
+        // récupère la liste pour le paienr du serveur
         this.$http.get('myCart')
             .then((response) => {
                 console.log("reponse du panier serveur", response.data);
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var fs = require('fs');
 
 var stockServer, cartServer, users;
 
+//lis le ficheir stock.json pour charger la liste
 fs.readFile('stock.json', (err,data)=>{
     if(err){
         console.log(err);
@@ -12,6 +13,7 @@ fs.readFile('stock.json', (err,data)=>{
     }
 })
 
+//lis le ficheir cart.json pour charger la liste
 fs.readFile('cart.json', (err, data) => {
     if (err) {
         console.log(err);
@@ -20,6 +22,7 @@ fs.readFile('cart.json', (err, data) => {
     }
 })
 
+//lis le ficheir users.json pour charger la liste
 fs.readFile('users.json', (err, data) => {
     if (err) {
         console.log(err);
@@ -35,21 +38,27 @@ router.get('/', function (req, res, next) {
     });
 });
 
+// GET stock list
+// route pour envoyer la liste stockServer au client quand il la demande
 router.get('/myStock', (req, res) => {
     console.log(JSON.stringify(users))
     res.json(stockServer)
 });
 
+//GET cart list
+// route pour envoyer la liste cartServer au client quand il la demande
 router.get('/myCart', (req, res) => {
     res.json(cartServer)
 });
 
-
+// POST cart list
+// route pour recuperer les valeur à traiter pour la liste cartServer
 router.post('/myCart', (req, res) => {
     var id = req.body.id;
     if (stockServer[id].quantity >= 1) {
         cartServer[id].quantity += 1;
         stockServer[id].quantity -= 1;
+        // ecrire la nouvelle valeur dans le stock.json
         fs.writeFile("stock.json", JSON.stringify(stockServer), err => {
             if (err) {
                 console.log(err)
@@ -63,12 +72,15 @@ router.post('/myCart', (req, res) => {
     }
 })
 
+// POST stock list
+// route pour recuperer les valeur à traiter pour la liste serverStock
 router.post('/myStock', (req,res) =>{
     var id = req.body.id;
     console.log('id', id)
     if(cartServer[id].quantity >= 1){
         cartServer[id].quantity -= 1;
         stockServer[id].quantity += 1;
+        // ecrire la nouvelle valeur dans le stock.json
         fs.writeFile("stock.json", JSON.stringify(stockServer), err => {
             if (err) {
                 console.log(err)
@@ -82,7 +94,8 @@ router.post('/myStock', (req,res) =>{
     }
 })
 
-
+// POST create user
+// route pour recuperer les valeurs à traiter pour creer un user
 router.post('/createUser', (req, res) => {
     users.push({
         firstName: req.body.firstName,
@@ -91,6 +104,7 @@ router.post('/createUser', (req, res) => {
         email: req.body.email,
         password: req.body.password
     })
+    // enregistre le nouvel user dans users.json
     fs.writeFile("users.json", JSON.stringify(users), err => {
         if(err){
             console.log(err)
@@ -101,10 +115,14 @@ router.post('/createUser', (req, res) => {
     res.status(200).send('User enregistré');
 })
 
+// POST Login
+// route pour recuperer les valeurs pour valider l'authentification
 router.post('/login', (req,res) =>{
     var user = req.body.email;
     var password = req.body.password;
+    // parcous le tableau des users
     users.forEach(item =>{
+        //validation du l'authentification si l'email et le password corresponde bien
         if(item.email === user && item.password === password){
             res.status(200).send("OK");
         } else {
